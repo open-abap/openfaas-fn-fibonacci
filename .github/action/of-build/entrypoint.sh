f88fc04691f81450ae639606fac09ed2e5b630a8
mrsimpson <github@beimir.net>
2020-03-03 01:41:16 +0100

    --

--- .github/action/of-build/entrypoint.sh
+++ .github/action/of-build/entrypoint.sh
@@ -39,7 +39,7 @@ echo $3 | docker login -u $2 --password-stdin $REGISTRY
 # Build and push
 
 cd "build/$(ls build | sed -n p)"
-docker buildx create --driver docker-container --use
+docker buildx create --use
 docker buildx install
 docker build --platform $4 -t $IMAGE_FULL --push .
 
